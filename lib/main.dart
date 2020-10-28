5cde2310c318f61c06b535c3a943413ff25cf13c
unknown <aryan.clipsy@gmail.com>
2020-10-28 21:27:08 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:movie_app/screens/movies/movie_details.dart';
 import 'package:movie_app/screens/movies/movie_list.dart';
@@ -11,6 +9,7 @@ import 'package:movie_app/services/lists/buildlist2.dart';
 import 'package:movie_app/services/lists/buildlist3.dart';
 import 'package:movie_app/services/drawer.dart';
 import 'package:movie_app/services/movie.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -19,16 +18,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // home: MovieDetails() ,
-      initialRoute: '/',
-      routes: {
-        '/':(context) => MovieList(),
-        '/moviedetails' :(context) => MovieDetails(),
-        
-
-      }
-      
-         );
+        debugShowCheckedModeBanner: false,
+        // home: MovieDetails() ,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => MovieList(),
+          '/moviedetails': (context) {return  MovieDetails();},
+        });
   }
 }
